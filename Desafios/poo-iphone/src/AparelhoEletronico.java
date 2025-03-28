2d61878e4ebecd6b4534aee23f341d81b9d3a91c
Mango Soda <4loneshimono@gmail.com>
2025-03-28 20:40:52 -0300

    Classe Smartphone apagada

--- Desafios/poo-iphone/src/AparelhoEletronico.java
+++ Desafios/poo-iphone/src/AparelhoEletronico.java
@@ -1,10 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class AparelhoEletronico {
     private boolean ligado;
     private boolean conectado;
+    private boolean chamadoRecebido;
+    private Map<String, String> mensagensDeVoz;
 
     public AparelhoEletronico() {
         this.ligado = false;
         this.conectado = false;
+        this.chamadoRecebido = false;
+        this.mensagensDeVoz = new HashMap<>();
     }
 
     public void ligar() {
@@ -28,5 +35,49 @@ public class AparelhoEletronico {
     public void setConexao(boolean conectado) {
         this.conectado = conectado;
         System.out.printf("A conexão foi %s.\n", (conectado ? "estabelecida" : "desconectada"));
+    }
+
+    public void receberChamada() {
+        if (!ligado) {
+            System.out.println("O telefone está desligado. Ligue antes de atender.");
+            return;
+        }
+
+        chamadoRecebido = true;
+        System.out.println("Chamada recebida.");
+    }
+
+    public void atender() {
+        if (chamadoRecebido) {
+            System.out.println("Chamada recebida.");
+        } else {
+            System.out.println("Sem chamada para atender.");
+        }
+    }
+
+    public void encerrarChamada() {
+        chamadoRecebido = false;
+        System.out.println("Chamada encerrada.");
+    }
+
+    public void iniciarCorreioVoz() {
+        if (!ligado) {
+            System.out.println("O telefone está desligado. Não há mensagens de voz disponíveis.");
+            return;
+        }
+
+        System.out.println("Você tem mensagens de voz:");
+        for (Map.Entry<String, String> entry : mensagensDeVoz.entrySet()) {
+            System.out.println("Contato: " + entry.getKey() + "\nMensagem: " + entry.getValue());
+        }
+    }
+
+    public void adicionarMensagem(String contato, String mensagem) {
+        if (contato == null || contato.isEmpty() || mensagem == null || mensagem.isEmpty()) {
+            System.out.println("Erro: Não foi possível adicionar o contato ou mensagem");
+            return;
+        }
+        System.out.println("Contato e mensagem adicionados com sucesso.");
+        mensagensDeVoz.put(contato, mensagem);
     }
 }
